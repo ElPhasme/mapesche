9146b6088b77032b9f9d1c166ac5c0dc2e8bdfce
ElPhasme <104252798+ElPhasme@users.noreply.github.com>
2022-05-12 16:11:13 +0200

    switch to PHP

---
+++ footer.php
@@ -0,0 +1,17 @@
+
+<footer>
+<div class="reseaux_sociaux arrivee">
+	<a href="https://soundcloud.com/mapesche"><img src="images/soundcloud1.svg" alt="Soundcloud"></a>
+	<a href="https://fr.linkedin.com/in/mariellepeschet"><img src="images/linkedin.svg" alt="LinkedIn"></a>
+	<a href="https://www.youtube.com/watch?v=rB-owgXag4I" ><img src="images/instagram.svg" alt="Instagram">
+	</a>
+</div>
+
+<div class="mentions_legales arrivee">
+	<a href="mentionslegales.html">
+        mapesche &#169;  2022</a>
+</div>
+</footer>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include("header.php");
+?>
+
+<main class="section_centre">
+
+	<img id="logo_central" src="images/logo_vec.svg" alt="logo mapesche voix off">
+
+	<h1 class="arrivee">Marielle Peschet <br/>
+	 VOIX OFF
+	</div>
+
+</main>
+
+<?php
+include("footer.php");
+?>
